72c8c7c15b6b748a93ac475474d1707f6cba8702
nguyenngocdue <68887000+nguyenngocdue@users.noreply.github.com>
2023-07-22 07:06:08 +0700

    test create sheets

--- test.py
+++ test.py
@@ -1,208 +1,253 @@
+import os
 import clr
+clr.AddReference("System")   
 import System
-import math
-from System.Collections.Generic import *
-
-clr.AddReference("ProtoGeometry")
-from Autodesk.DesignScript.Geometry import *
-
-clr.AddReference("RevitAPI")
+from System.ComponentModel import BackgroundWorker
+clr.AddReference('RevitAPI')
 import Autodesk
-from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import*
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector
+from System.Collections.Generic import List
 
-clr.AddReference("RevitAPIUI")
-from Autodesk.Revit.UI import *
-from Autodesk.Revit.UI.Selection import *
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import*
+from  Autodesk.Revit.UI.Selection import*
 
-clr.AddReference("RevitNodes")
-import Revit
-
-clr.ImportExtensions(Revit.Elements)
-clr.ImportExtensions(Revit.GeometryConversion)
 
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
+#-------------------------------------------------------------------
+# WPF
+try:
+    clr.AddReference("IronPython.wpf")
+    clr.AddReference('PresentationCore')
+    clr.AddReference('PresentationFramework')
+except IOError:
+    raise
+from System.IO import StringReader
+from System.Windows.Markup import XamlReader, XamlWriter
+from System.Windows import Window, Application, MessageBox, MessageBoxButton, MessageBoxResult
+from System.Windows import RoutedEventHandler
+from System.Runtime.InteropServices import Marshal
+from System.Collections.ObjectModel import ObservableCollection
+from System.Windows.Data import ListCollectionView
+try:
+    import wpf
+except ImportError:
+    raise
 
-import os
-import sys
-import logging
-sys.path.append(r"C:\\Users\\NGUYEN NGOC DUE\\.vscode\\ironpython-stubs-master\\release\\stubs.min\\pyrevit\\coreutils")
-sys.path.append(os.path.dirname(__file__))
+#-------------------------------------------------------------------
+def logger(title, content):
+    import datetime
+    date = datetime.datetime.now()
+    f = open(r"A:\Library-Dynamo-Python-CSharp\python.log", 'a')
+    f.write(str(date) + '\n' + title + '\n' + str(content) + '\n')
+    f.close()
+
+def getDataFromRows(rows_count, cols_count, start_row = 1, start_col = 1):
+    data = []
+    for row_index in range(start_row, rows_count + 1):
+        colData = []
+        for col_index in range(start_col, cols_count + 1):
+            cell_value = str(workSheet.Cells[row_index, col_index].Value2)
+            colData.append(cell_value)
+        data.append(colData)
+    return data
 
 
-
+def getDataFromColumns(rows_count, cols_count, start_row=1, start_col=1):
+    data = []
+    for col_index in range(start_col, cols_count + 1):
+        colData = []
+        for row_index in range(start_row, rows_count + 1):
+            cell_value = str(workSheet.Cells[row_index, col_index].Value2)
+            colData.append(cell_value)
+        data.append(colData)
+    return data
+#-------------------------------------------------------------------
 doc = DocumentManager.Instance.CurrentDBDocument
 view = doc.ActiveView
 uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+DB = Autodesk.Revit.DB
 
-from Autodesk.Revit.DB import XYZ
-class PointCalculator:
-    def __init__(self, point_pairs):
-        """
-        Initializes PointCalculator with a list of point pairs.
-        Args:
-            point_pairs (list): The list of point pairs. Each pair contains two points (point1, point2).
-        """
-        self.point_pairs = point_pairs
+def getSheetTBlocks(src_sheet):
+    sheet_tblocks = FilteredElementCollector(src_sheet.Document, src_sheet.Id) \
+                    .OfCategory(BuiltInCategory.OST_TitleBlocks) \
+                    .WhereElementIsNotElementType() \
+                    .ToElements()
+    if sheet_tblocks:
+        title_block_name = sheet_tblocks[0].Name
+        return title_block_name
+    return None
 
-    @property
-    def calculate_midpoints(self):
-        """
-        Calculates the midpoints for each point pair.
-        Returns:
-            list: The list of corresponding midpoints (XYZ objects).
-        """
-        midpoints = []
-        for pair in self.point_pairs:
-            point1, point2 = pair
-            x = (point1.X + point2.X) / 2
-            y = (point1.Y + point2.Y) / 2
-            z = (point1.Z + point2.Z) / 2
-            midpoint = XYZ(x, y, z)
-            midpoints.append(midpoint)
-        return midpoints
+def getAllTitleBlocks():
+    titleBlockFilter = ElementCategoryFilter(BuiltInCategory.OST_TitleBlocks)
+    collector = FilteredElementCollector(doc).WherePasses(titleBlockFilter).WhereElementIsNotElementType()
+    titleBlockList = []
+    for titleBlock in collector:
+        titleBlockList.append(titleBlock)
+    return titleBlockList
 
-    @property
-    def left_points(self):
-        """
-        Returns the left points in each point pair.
-        Returns:
-            list: The list of left points (XYZ objects).
-        """
-        left_points = [pair[0] for pair in self.point_pairs]
-        return left_points
+def createSheet(sheet_num, sheet_name,
+                 titleBlock_id=DB.ElementId.InvalidElementId, doc=None):
+    doc = DocumentManager.Instance.CurrentDBDocument
+    TransactionManager.Instance.EnsureInTransaction(doc)
+    newSheet = DB.ViewSheet.Create(doc, titleBlock_id)
+    TransactionManager.Instance.TransactionTaskDone()
+    newSheet.Name = sheet_name
+    newSheet.SheetNumber = sheet_num
+    return newSheet
 
-    @property
-    def right_points(self):
-        """
-        Returns the right points in each point pair.
-        Returns:
-            list: The list of right points (XYZ objects).
-        """
-        right_points = [pair[1] for pair in self.point_pairs]
-        return right_points
+def getAllSheets():
+    collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType()
+    sheetList = [] 
+    for sheet in collector:
+        sheetList.append(sheet)
+    return sheetList
 
-    @property
-    def distance(self):
-        """
-        Calculates the distance between each point pair.
-        Returns:
-            list: The list of distances between the points.
-        """
-        distances = []
-        for pair in self.point_pairs:
-            point1, point2 = pair
-            distance = point1.DistanceTo(point2)
-            distances.append(distance)
-        return distances
+def getSheetInfoDictionary():
+    collector = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType()
+    sheetInfoDict = {}
+    sheetNames = []
+    sheetNumbers = []
+    titleBlocks = []
+    for sheet in collector:
+        sheetNames.append(sheet.Name)
+        sheetNumbers.append(sheet.SheetNumber)
+        titleBlock = getSheetTBlocks(sheet)
+        if not titleBlock: titleBlock = None
+        titleBlocks.append(titleBlock)
+        sheetInfoDict['sheet_names'] = sheetNames
+        sheetInfoDict['sheet_numbers'] = sheetNumbers
+        sheetInfoDict['title_blocks'] = titleBlocks
+    return sheetInfoDict
 
-    @property
-    def max_point(self):
-        """
-        Finds the maximum point among all points.
-        Returns:
-            XYZ: The maximum point.
-        """
-        all_points = [point for pair in self.point_pairs for point in pair]
-        max_x = max(point.X for point in all_points)
-        max_y = max(point.Y for point in all_points)
-        max_z = max(point.Z for point in all_points)
-        max_point = XYZ(max_x, max_y, max_z)
-        return max_point
+#-------------------------------------------------------------------
+# Read data from Excel into a 2D list
+excel = Marshal.GetActiveObject("Excel.Application")
+excel.Visible = True
+excel.DisplayAlerts = False
+filePath = r'A:\TRAINING DYNAMO API\PART 26 CURD Sheet\InforSheet.xlsx'
+workbook = excel.Workbooks.Open(filePath)
+workSheet = workbook.ActiveSheet
 
-    @property
-    def min_point(self):
-        """
-        Finds the minimum point among all points.
-        Returns:
-            XYZ: The minimum point.
-        """
-        all_points = [point for pair in self.point_pairs for point in pair]
-        min_x = min(point.X for point in all_points)
-        min_y = min(point.Y for point in all_points)
-        min_z = min(point.Z for point in all_points)
-        min_point = XYZ(min_x, min_y, min_z)
-        return min_point
+# Get the used range of the worksheet (including only the data)
+usedRange = workSheet.UsedRange
 
-    @property
-    def sort_points(self, axis="X"):
-        """
-        Sorts the points in each point pair in ascending order based on the specified axis.
-        Args:
-            axis (str): The axis to sort the points ("X", "Y", or "Z"). Default is "X".
-        """
-        for pair in self.point_pairs:
-            pair.sort(key=lambda point: getattr(point, axis))
+# Get the number of rows and columns in the used range
+rows_count = usedRange.Rows.Count
+cols_count = usedRange.Columns.Count
+start_row = 1
+start_col = 2
+# Read data from each row separately
+dataOfRows = getDataFromRows(rows_count, cols_count, start_row, start_col)
+dataOfCols = getDataFromColumns(rows_count, cols_count, start_row, start_col)
 
-    @property
-    def get_midpoint_index(self, midpoint):
-        """
-        Returns the index of the given midpoint in the list of midpoints.
-        Args:
-            midpoint (XYZ): The midpoint to find the index of.
-        Returns:
-            int: The index of the midpoint. Returns -1 if not found.
-        """
-        midpoints = self.calculate_midpoints
-        for i, point in enumerate(midpoints):
-            if point == midpoint:
-                return i
-        return -1
+excel.Visible = True
+Marshal.ReleaseComObject(excel)
+
+keys = ['sheet_numbers','sheet_names', 'title_blocks']
+
+dictInfoSheets = {k : i[1:] for i , k in zip(dataOfCols, keys)}
+keysInfoSheets = dictInfoSheets.keys()
+sheetNumbers = dictInfoSheets[keysInfoSheets[0]]
+sheetNames =  dictInfoSheets[keysInfoSheets[1]]
+titleBlocks = dictInfoSheets[keysInfoSheets[2]]
+allTitleBlocks = getAllTitleBlocks()
+
+sheetInfoDictionary = getSheetInfoDictionary()
+sheetNumbersOnRevit = sheetInfoDictionary[keys[0]]
+
+# ----------------------------------------------------------------
+#FORM
+
+class ViewModel():
+    def __init__(self, cgv1, cgv2, cgv3, cgv4):
+        self._gvNo = cgv1
+        self._gvSheetNumber = cgv2
+        self._gvSheetName = cgv3
+        self._gvTitleBlock = cgv4
 
     @property
-    def create_vectors(self):
-        """
-        Creates vectors from left to right for each point pair.
-        Returns:
-            list: The list of corresponding vectors (XYZ objects).
-        """
-        vectors = []
-        for pair in self.point_pairs:
-            point1, point2 = pair
-            vector = point2 - point1
-            vectors.append(vector)
-        return vectors
+    def gvSheetName(self):
+        return self._gvSheetName
+    @property
+    def gvSheetNumber(self):
+        return self._gvSheetNumber
+    @property
+    def gvTitleBlock(self):
+        return self._gvTitleBlock    
+    @property
+    def gvNo(self):
+        return self._gvNo
 
-    @staticmethod
-    def create_lines( point_pairs, doc):
-        """
-        Creates lines from each point pair in the Revit document.
-        Args:
-            doc (Document): The Revit document to create the lines in.
-        Returns:
-            list: The list of created Line objects.
-        """
-        TransactionManager.Instance.EnsureInTransaction(doc)
-        midpoints = PointCalculator.calculate_midpoints
-        lines = []
-        for pair in point_pairs:
-            point1, point2 = pair
-            start_point = XYZ(point1.X, point1.Y + 1, point1.Z)
-            end_point = XYZ(point2.X, point2.Y + 1 , point2.Z)
-            line = Line.CreateBound(start_point, end_point)
-            lines.append(line)
-            doc.Create.NewDetailCurve(doc.ActiveView, line)
-        TransactionManager.Instance.TransactionTaskDone()
-        return lines
+class MyWindow(Window):
+    def __init__(self, dataModels):
+        self.ui = wpf.LoadComponent(self, r'A:\TRAINING DYNAMO API\PART 26 CURD Sheet\Forms\CURD_SHEETS\CURD_SHEETS\MainWindow.xaml')
+        self.selectedElements = []
+        
+        
+        # for k, v in dataModels.items():
+        #     control = self.FindName(k)
+        #     if control is not None:
+        #         control.ItemsSource = v
 
-point_pairs = [xyz for xyz in IN[0]]
+        
 
-# Create an instance of PointCalculator with the point pairs
-calculator = PointCalculator(point_pairs)
+    def btnRefresh(self, sender, event):
+        pass
+    def btnUploadExcel(self, sender, event):
+        pass
+    def btnExploreExcel(self, sender, event):
+        pass
+    def btnCreateSheets(self, sender, event):
+        sheets = []
+        try:
+            for num, name, titleBlock in zip(dictInfoSheets['sheet_numbers'], dictInfoSheets['sheet_names'], dictInfoSheets['title_blocks']):
+                logger('202',  sheetNumbersOnRevit)
+                if str(num) in sheetNumbersOnRevit: continue
+                for t in allTitleBlocks:
+                    if t.Name == titleBlock:
+                        sheet = createSheet(num, name)
+                        sheets.append(sheet)
+            self.Hide()
+            # After creating sheets, refresh the Revit UI to display the changes
+            uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+            uidoc.RefreshActiveView()
+            MessageBox.Show( 'sheets were created')
+            self.lvSheetsRevit.Items.Refresh()
+            self.Show()
+        except Exception :
+            self.UpdateLayout()
+            pass
+    def btnCancel(self, sender, event):
+        self.Close()
 
-# Calculate the midpoints
-midpoints = calculator.calculate_midpoints
-distance = calculator.distance
-dbLines = PointCalculator.create_lines(point_pairs,doc)
+def createModel(dictData):
+    viewModels = []
+    keys = dictData.keys()
+    for i in range(len(dictData[keys[0]])):
+        viewModel = ViewModel(
+            cgv1=i + 1,
+            cgv2=dictData[keys[0]][i],
+            cgv3=dictData[keys[2]][i],
+            cgv4=dictData[keys[1]][i],
+        )
+        viewModels.append(viewModel)
+    return viewModels
+
+viewModelSheetsRevit = createModel(sheetInfoDictionary)
+viewModelSheetsExcel = createModel(dictInfoSheets)
+dataModels = {'lvSheetsRevit' : viewModelSheetsRevit, 'lvSheetsExcel': viewModelSheetsExcel}
+
+OUT = sheetInfoDictionary
+
+myWindow = MyWindow(dataModels)
+myWindow.ShowDialog()
 
 
-# Print the results
 
-result = []
 
-for i, dis in enumerate(dbLines):
-    result.append(dis)
-OUT = result
